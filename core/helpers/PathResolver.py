15411897f9bb9389eea774d5092e09a4614b95b4
Mirko Boehm <mirko@kdab.com>
2010-07-22 19:52:16 +0200

    Add PathResolver that determines project-local paths during project build time.

---
+++ core/helpers/PathResolver.py
@@ -0,0 +1,46 @@
+# This file is part of make-o-matic.
+# -*- coding: utf-8 -*-
+# 
+# Copyright (C) 2010 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+# Author: Mirko Boehm <mirko@kdab.com>
+# 
+# make-o-matic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# make-o-matic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+from core.helpers.TypeCheckers import check_for_nonempty_string_or_none
+
+class PathResolver( object ):
+	'''A path resolver resolves a project filename to its full path at the time it is converted to a string.
+	It calls a function that must return a path, and extends that path with the filename.'''
+
+	def __init__( self, function, filename ):
+		self.setFunction( function )
+		self.setFilename( filename )
+
+	def setFilename( self, filename ):
+		check_for_nonempty_string_or_none( filename, 'The filename has to be a non-empty string, or None!' )
+		self.__filename = filename
+
+	def getFilename( self ):
+		return self.__filename
+
+	def setFunction( self, function ):
+		# FIXME how to check that function is callable?
+		self.__function = function
+
+	def getFunction( self ):
+		return self.__function
+
+	def __str__( self ):
+		path = os.path.join( self.getFunction()(), self.getFilename() )
+		return path
