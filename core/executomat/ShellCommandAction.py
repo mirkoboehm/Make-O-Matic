7b52d056754340c7da3b2671d02f98665d8ae720
Mirko Boehm <mirko@kdab.net>
2010-07-25 17:54:36 +0200

    Add proper license information.

--- core/executomat/ShellCommandAction.py
+++ core/executomat/ShellCommandAction.py
@@ -1,4 +1,22 @@
-# This file implements the ShellCommandAction class
+# This file is part of make-o-matic.
+# -*- coding: utf-8 -*-
+# 
+# Copyright (C) 2010 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+# Author: Mirko Boehm <mirko@kdab.com>
+# 
+# make-o-matic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# make-o-matic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from core.helpers.TypeCheckers import check_for_nonempty_string, check_for_nonnegative_int
 from core.Exceptions import MomError
 from core.helpers.RunCommand import RunCommand
