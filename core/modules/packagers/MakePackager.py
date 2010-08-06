40cca34921c6d678245c8649a141955a6ef2d520
Mike McQuaid <mike@mikemcquaid.com>
2010-08-06 16:04:27 +0100

    Correct MakePackager's imports.

--- core/modules/packagers/MakePackager.py
+++ core/modules/packagers/MakePackager.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from core.modules.PackageProvider import PackageProvider
+from core.modules.packagers.PackageProvider import PackageProvider
 from core.executomat.ShellCommandAction import ShellCommandAction
 
 class MakePackager( PackageProvider ):
