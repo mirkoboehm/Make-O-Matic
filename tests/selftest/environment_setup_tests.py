fcadbf96aec2de6f0e1905a1dc815ed3aa8db7e5
Kevin Funk <kevin.funk@kdab.com>
2010-11-05 14:32:41 +0100

    Add missing file, sorry

---
+++ tests/selftest/environment_setup_tests.py
@@ -0,0 +1,30 @@
+# This file is part of Make-O-Matic.
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+# Author: Kevin Funk <krf@electrostorm.net>
+# 
+# Make-O-Matic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Make-O-Matic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+from tests.helpers.MomTestCase import MomTestCase
+
+class EnvironmentSetupTests( MomTestCase ):
+
+	def testIfTestEnvironmentVariableIsSet( self ):
+		self.assertTrue( os.getenv( "MOM_TESTS_RUNNING" ) == "1" )
+
+if __name__ == "__main__":
+	unittest.main()
