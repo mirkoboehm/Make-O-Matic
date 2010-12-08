f9b46e2a98fe31b6d9f46307b8f7d518125f6ac4
Mike McQuaid <mike@mikemcquaid.com>
2010-12-08 12:23:37 +0000

    Try another approach for fixing rmtree on Windows.

--- core/helpers/SafeDeleteTree.py
+++ core/helpers/SafeDeleteTree.py
@@ -18,16 +18,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import shutil
-import time
+import errno
+import stat
 
 def rmtree( path ):
+	def rmtreeErrorHandler( function, path, exception ):
+		error = exception[1].errno
+		if function in ( os.rmdir, os.remove ) and error == errno.EACCES:
+			# Set permissions to allow all users read/write access to the file
+			os.chmod( path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO )
+			function( path )
+
 	if os.path.exists( path ):
-		cwd = os.getcwd()
-		realpath = os.path.realpath( path )
-		os.chdir( os.path.expanduser( '~' ) )
-		#FIXME Try and delete path twice (if needed) to work around stupid Windows race condition
-		shutil.rmtree( realpath, True )
-		if os.path.exists( realpath ):
-			time.sleep( 15 )
-			shutil.rmtree( realpath, False )
-		os.chdir( cwd )
+			shutil.rmtree( path, ignore_errors = False, onerror = rmtreeErrorHandler )
