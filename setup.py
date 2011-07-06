5fb1b6ff078aaff78e6136b6411aef33e85dd775
Mike McQuaid <mike.mcquaid@kdab.com>
2011-07-06 15:54:24 +0100

    Add first attempt at setuptools setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+	name = "Make-O-Matic",
+	version = "0.1.0",
+	author = "KDAB",
+	author_email = "make-o-matic-support@kdab.com",
+	description = ("Maker of matics"),
+	license = "GPL",
+	keywords = "make-o-matic",
+	url = "https://github.com/KDAB/Make-O-Matic",
+	packages = ['buildcontrol', 'core', 'tests', 'tools'],
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"Environment :: Console",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Programming Language :: Python",
+		"Topic :: Software Development :: Build Tools",
+	],
+	install_requires = ['lxml'],
+	entry_points = { 
+		"console_scripts": [
+			"mom = tools.mom:main",
+			"mom-report-converter = tools.report_converter:main",
+			"mom-ci = tools.simple_ci:main",
+			"mom-test = tests.testsuite_selftest:main",
+		],
+	},
+)
