f0255fe85924663a52e8ff76cf07f7f1749198db
Kevin Funk <kevin.funk@kdab.com>
2011-07-26 12:33:12 +0200

    WIP: Fix another conflict in ProxyBuilder

--- mom/apps/ProxyBuilder.py
+++ mom/apps/ProxyBuilder.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import
+
 from mom.apps.mom.Remotebuilder import RemoteBuilder
 from mom.core.BuildParameters import BuildParameters
 from mom.core.MApplication import MApplication
